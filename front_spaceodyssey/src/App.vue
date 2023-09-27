b46b8bb820deffd6b67556af7e5b99a236322b8c
Maxime Portalier <maximeportalier@hotmail.fr>
2023-09-27 14:51:31 +0200

    ADD [Front] First page

--- front_spaceodyssey/src/App.vue
+++ front_spaceodyssey/src/App.vue
@@ -3,15 +3,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld 
+  title="Space Odyssey"
+  subtitle="Découvrez le secret universel à travers un voyage au cœur de l’espace"
+  />
 </template>
 
 <style scoped>
